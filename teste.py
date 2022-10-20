f07c29b542d6066c72f4528ef999262dcfb98bbd
Erick Silva <71526173+ErickSilvaDeAlmeida@users.noreply.github.com>
2022-10-20 09:31:31 -0300

    att

---
+++ teste.py
@@ -0,0 +1,7 @@
+dia_ini = 24
+dia_fim = 28
+mes = "fevereiro"
+ano = 2017
+
+print(f"Em {ano} o carnaval acontece em {mes}, do dia {dia_ini} ao dia {dia_fim}.")
+#print("Em {} o carnaval acontece em {}, do dia {} ao dia {}.".format(ano, mes, dia_ini, dia_fim))
